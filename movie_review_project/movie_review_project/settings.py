931467747e054215226db39ab4c8eb1f6147fb01
Alexey Gorshenin <augomgtu@gmail.com>
2024-10-08 22:46:14 +0600

    Fix

--- movie_review_project/movie_review_project/settings.py
+++ movie_review_project/movie_review_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['augomgtu.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 # Секретный ключ
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
